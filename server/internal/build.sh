0a733a1322f927af1b02c896fec9e93a81d2828a
redundant4u <physiogel@pusan.ac.kr>
2024-04-16 14:04:15 +0900

    chore: internal 코드 분리

--- server/internal/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-./gradlew build
-
-cp build/libs/*.jar ../member/libs
-cp build/libs/*.jar ../auth/libs
-
-../member/gradlew --refresh-dependencies
-../auth/gradlew --refresh-dependencies
