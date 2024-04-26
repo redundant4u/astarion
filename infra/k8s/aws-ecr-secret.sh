9c2caa029a9fd360e19d8716afdfe9b4b5905fcb
redundant4u <physiogel@pusan.ac.kr>
2024-04-26 13:26:44 +0900

    chore: infra 코드 삭제

--- infra/k8s/aws-ecr-secret.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-kubectl create secret docker-registry ${SECRET_NAME} \
-    --docker-server=https://${ACCOUNT}.dkr.ecr.${REGION}.amazonaws.com \
-    --docker-username=AWS \
-    --docker-password="${TOKEN}" \
-    --docker-email="${EMAIL}"
